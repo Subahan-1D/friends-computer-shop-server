493ef2ea3474d01a2fe07926f419264b4a369d43
subahan ali <subahanislam523@gmail.com>
2024-07-14 10:41:55 -0600

    get all item get user by email

--- index.js
+++ index.js
@@ -29,53 +29,76 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     //const jobsCollection = client.db('soloSphere').collection('jobs')
-    const shopsCollection = client.db('friendsComputer').collection('serviceItem');
-    const bidsCollection = client.db('friendsComputer').collection('serviceBids');
+    const shopsCollection = client
+      .db("friendsComputer")
+      .collection("serviceItem");
+    const bidsCollection = client
+      .db("friendsComputer")
+      .collection("serviceBids");
 
     //Get all ServicesItem data from db
-    app.get('/serviceItem',async(req,res)=>{
-        const result = await shopsCollection.find().toArray()
-        res.send(result)
-    })
-    //get a single data from database 
-    app.get('/item/:id',async(req,res)=>{
-        const id = req.params.id ;
-        const query = {_id : new ObjectId(id)}
-        const result = await shopsCollection.findOne(query)
-        res.send(result)
-    })
+    app.get("/serviceItem", async (req, res) => {
+      const result = await shopsCollection.find().toArray();
+      res.send(result);
+    });
+    //get a single data from database
+    app.get("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await shopsCollection.findOne(query);
+      res.send(result);
+    });
 
     // save a item data in db
-    app.post('/item',async(req,res)=>{
+    app.post("/item", async (req, res) => {
       const itemData = req.body;
-      const result = await bidsCollection.insertOne(itemData)
-      res.send(result)
-
+      const result = await bidsCollection.insertOne(itemData);
+      res.send(result);
     });
     // save a query service item data in db
-    app.post('/query',async(req,res)=>{
+    app.post("/query", async (req, res) => {
       const queryData = req.body;
-      const result = await shopsCollection.insertOne(queryData)
-      res.send(result)
-
-    })
+      const result = await shopsCollection.insertOne(queryData);
+      res.send(result);
+    });
 
     // save a all service item a speacic user
-       app.get("/serviceItems/:email", async (req, res) => {
-         const email = req.params.email;
-         const query = { "user_Info.email": email };
-         const result = await shopsCollection.find(query).toArray();
-         res.send(result);
-       });
+    app.get("/serviceItems/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { "user_Info.email": email };
+      const result = await shopsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-       // delete a data from db
-       app.delete('/item/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query ={_id: new ObjectId(id)}
-        const result = await shopsCollection.deleteOne(query)
-        res.send(result)
-       })
-      
+    // delete a data from db
+    app.delete("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await shopsCollection.deleteOne(query);
+      res.send(result);
+    });
+    // update data from db
+     app.put("/item/:id", async (req, res) => {
+       const id = req.params.id;
+       const itemData = req.body;
+       const query = { _id: new ObjectId(id) };
+       const options = { upsert: true };
+       const updateDoc = {
+         $set: {
+           ...itemData,
+         },
+       };
+       const result = await shopsCollection.updateOne(query, updateDoc, options);
+       res.send(result);
+     });
+
+     // get all item get user by email
+     app.get('/recommended-me/:email', async(req,res)=>{
+      const email = req.params.email;
+      const query = {email}
+      const result = await bidsCollection.find(query).toArray()
+      res.send(result)
+     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -83,7 +106,6 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
